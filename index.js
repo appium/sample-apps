632b97cce16d990c38da62336ca3f2341807c70d
Jonah Stiennon <jonahss@gmail.com>
2014-10-06 16:42:14 -0700

    added gappium

--- index.js
+++ index.js
@@ -9,10 +9,40 @@ var lastFile = function(fullPath) {
   return _.last(fullPath.split(path.sep));
 }
 
+var getOptionalDeps = function() {
+  return Object.keys(require('./package.json').optionalDependencies);
+}
+
+var appPathsForModule = function(module) {
+  var moduleEntrypoint = require.resolve(module);
+
+  if (_.last(moduleEntrypoint.split('.')) == 'json') {
+    var moduleBasePath = moduleEntrypoint.split(path.sep).slice(0, -1).join(path.sep);
+    var paths = require(module);
+    return paths.map(function(appPath) {
+      return moduleBasePath + path.sep + path.normalize(appPath);
+    });
+  }
+  else {
+    return [moduleEntrypoint];
+  }
+
+}
+
+// returns an array of all app files pointed to within the package.json's optionalDependencies
+// for most apps this is equivalent to the 'main' entrypoint for the module
+// some modules 'main' entrypoint is a .json file which yields a registry of multiple app binaries contained in the module, these are all returned.
+var getPathsFromOptionalDeps = function() {
+
+  var unflattened = getOptionalDeps().map(appPathsForModule);
+  return _.flatten(unflattened);
+}
+
+
 var getApp = function(app) {
   // first search npm modules
-  var optionalDeps = Object.keys(require('./package.json').optionalDependencies);
-  optionalDeps = optionalDeps.map(require.resolve);
+  var optionalDeps = getPathsFromOptionalDeps();
+  optionalDeps = _.sortBy(optionalDeps, 'length');
   var appPath = _.find(optionalDeps, function(name) {
     return lastFile(name).indexOf(app) >= 0;
   });
@@ -48,8 +78,7 @@ var dropSuffix = function(name) {
 }
 
 module.exports.list = function() {
-  var optionalDeps = Object.keys(require('./package.json').optionalDependencies);
-  optionalDeps = _.compact(optionalDeps.map(require.resolve));
+  var optionalDeps = getPathsFromOptionalDeps();
   optionalDeps = optionalDeps.map(lastFile);
 
   var preBuilt = fs.readdirSync('./pre-built');
