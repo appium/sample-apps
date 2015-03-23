a8cf4bd5682a1615995ce47a35be112b10e733c5
Jonah Stiennon <jonahss@gmail.com>
2015-03-23 12:01:56 -0700

    v1.1.0

---
+++ test.js
@@ -0,0 +1,8 @@
+doit = require('./index.js');
+
+console.log(doit.list());
+
+console.log(doit('ApiDemos-debug'));
+console.log(doit('ContactManager'));
+console.log(doit('HelloGappium'));
+console.log(doit('HelloGappium-debug'));
